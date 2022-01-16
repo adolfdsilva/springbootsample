7bc498f1280ef5e2a05d8ebed4acb995cc0ae861
Adolf Dsilva <adolf.dsilva.spean@gmail.com>
2022-01-16 08:20:27 +0700

    First commit

---
+++ customer-insurance-info/src/main/java/com/assignment/customerinsuranceinfo/CustomerInsuranceInfoApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.customerinsuranceinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerInsuranceInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerInsuranceInfoApplication.class, args);
+	}
+
+}
---
+++ customer-insurance-info/src/test/java/com/assignment/customerinsuranceinfo/CustomerInsuranceInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.customerinsuranceinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerInsuranceInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
